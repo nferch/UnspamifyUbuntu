aa7f943de2a5a96bf930ceca4ccc59bcee3ac523
Nathan A. Ferch <nf@marginal.net>
2024-03-17 12:17:07 -0400

    chore: move to source package

--- fake-ubuntu-advantage-tools/buildPackage.sh
+++ fake-ubuntu-advantage-tools/buildPackage.sh
@@ -1,1 +1,19 @@
-dpkg -b ./src fake-ubuntu-advantage-tools.deb
+#!/usr/bin/env bash
+set -euo pipefail
+
+DEPS="dpkg-dev debhelper"
+
+for d in $DEPS; do
+  if [[ "$(dpkg --get-selections $d | awk '{print $2}')" != "install" ]]; then
+    echo "$d needed, please install"
+    exit 1
+  fi
+done
+
+if [[ -n "$DEB_SIGN_KEYID" ]]; then
+  dpkg_buildpackage_opts=""
+else
+  dpkg_buildpackage_opts="-us -uc"
+fi
+
+cd src && dpkg-buildpackage $dpkg_buildpackage_opts
